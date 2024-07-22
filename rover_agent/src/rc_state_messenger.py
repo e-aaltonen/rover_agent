04fe7b6ae5b193328422e9cc5aac4abfcdec4ac6
Esa Aaltonen <etaalton@gmail.com>
2024-07-22 11:27:59 +0300

    Fix dependencies for bunker_msgs/scout_msgs

--- rover_agent/src/rc_state_messenger.py
+++ rover_agent/src/rc_state_messenger.py
@@ -30,12 +30,23 @@ Requirements:
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy
+import ros2node.command
 from ros2topic.api import get_msg_class
 from std_msgs.msg import UInt8, Int8
 from rover_agent_msgs.msg import RCchannels
 from rcl_interfaces.srv import GetParameters, SetParameters
-from bunker_msgs.msg import BunkerRCState
-from scout_msgs.msg import ScoutRCState
+import ros2interface
+
+agilex_robot = "None"
+nodelist = rclpy.Node.get_node_names()
+if "/bunker" in nodelist:
+    agilex_robot = "Bunker"
+    from bunker_msgs.msg import BunkerRCState
+
+elif "/scout" in nodelist:
+    agilex_robot = "Scout"
+    from scout_msgs.msg import ScoutRCState
+
 from rover_agent_msgs.msg import RCchannels
 
 
@@ -88,8 +99,10 @@ class SWMessenger(Node):
         #self.declare_parameter("rc_status_topic", value="/bunker_rc_status")
         #self._rc_topic = self.get_parameter("rc_status_topic").get_parameter_value().string_value       
         
-        self.sub_rc = self.create_subscription(BunkerRCState, "/bunker_rc_status", self.callback_rc_status, 10)
-        self.sub_rc2 = self.create_subscription(ScoutRCState, "/scout_rc_status", self.callback_rc_status, 10)
+        if agilex_robot == "Bunker":
+            self.sub_rc = self.create_subscription(BunkerRCState, "/bunker_rc_status", self.callback_rc_status, 10)
+        if agilex_robot == "Scout":
+            self.sub_rc2 = self.create_subscription(ScoutRCState, "/scout_rc_status", self.callback_rc_status, 10)
         
     def callback_rc_status(self, msg):
         self.get_logger().info("{0}".format(msg.swa));
